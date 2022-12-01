9b6b6f4f426a251f70135b1c916b0909f0dd7cac
Dmitrii Bulatov <bulati484@gmail.com>
2022-12-01 21:30:42 +0300

    1.corrected loginPage.js, 2.done homePage.js till 'Получение текущих курсов валюты'

---
+++ public/homePage.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const logout = new LogoutButton();
+logout.action = function() {
+    ApiConnector.logout(serverResponse => {
+        if (serverResponse.success) {
+            location.reload();
+            return;
+        }
+    });
+}
+
+ApiConnector.current(serverResponse => {
+    //console.log(serverResponse);
+    if (serverResponse.success) {
+        ProfileWidget.showProfile(serverResponse.data);
+        return;
+    }
+})
+
+const rates = new RatesBoard();
+//console.log(rates);
+function currantRates () {
+    ApiConnector.getStocks(responseBody => {
+        if(responseBody.success) {
+            rates.clearTable();
+            rates.fillTable(responseBody.data);
+        }
+    })
+}
+csetInterval(ratesUpdate(), 60000);
--- public/loginPage.js
+++ public/loginPage.js
@@ -2,17 +2,16 @@
 
 const userForm = new UserForm();
 
-userForm.loginFormCallback = data => ApiConnector.login(data, callback => {
-    //response.success;
-    if (callback !== true) {
-        return userForm.setLoginErrorMessage(callback.error);
+userForm.loginFormCallback = data => ApiConnector.login(data, serverResponse => {
+    if (!serverResponse.success) {
+        return userForm.setLoginErrorMessage(serverResponse.error);
     }
     location.reload();
 });
 
-userForm.registerFormCallback = data => ApiConnector.login(data, callback => {
-    if (callback !== true) {
-        return userForm.setRegisterErrorMessage(callback.error);
+userForm.registerFormCallback = data => ApiConnector.login(data, serverResponse => {
+    if (!serverResponse.success) {
+        return userForm.setRegisterErrorMessage(serverResponse.error);
     }
     location.reload();
 });
