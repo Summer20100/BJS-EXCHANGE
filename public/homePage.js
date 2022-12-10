e5b9925427dcf152f60b499f1a3756143e9ec8c2
Dmitrii Bulatov <bulati484@gmail.com>
2022-12-10 09:46:05 +0300

    add correction and new one

--- public/homePage.js
+++ public/homePage.js
@@ -1,32 +1,81 @@
 'use strict';
 
+// Выход из личного кабинета
+
 const logout = new LogoutButton();
 logout.action = function() {
-    ApiConnector.logout(serverResponse => {
-        if (serverResponse.success) {
+    ApiConnector.logout(response => {
+        if (response.success) {
             location.reload();
-            return;
-        }
-    });
-}
-
-ApiConnector.current(serverResponse => {
-    //console.log(serverResponse);
-    if (serverResponse.success) {
-        ProfileWidget.showProfile(serverResponse.data);
-        return;
-    }
-})
-
-
-const rates = new RatesBoard();
-//console.log(rates);
-function currantRates () {
-    ApiConnector.getStocks(responseBody => {
-        if(responseBody.success) {
-            rates.clearTable();
-            rates.fillTable(responseBody.data);
         }
     })
 }
-setInterval(currantRates(), 60000);
+
+// Получение информации о пользователе
+
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+})
+
+//Получение текущих курсов валюты
+
+const rates = new RatesBoard();
+function currantRates() {
+    ApiConnector.getStocks(response => {
+        if(response.success) {
+            rates.clearTable();
+            rates.fillTable(response.data);
+        }
+    })
+}
+currantRates();
+setInterval(currantRates, 60000);
+
+// Операции с деньгами //
+
+// 1. Пополнение баланса
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = function (balanceInc) {
+    ApiConnector.addMoney(balanceInc, response => {
+        if (response.success) {
+            moneyManager.addMoneyAction();
+            ProfileWidget.showProfile(response.data);
+            return moneyManager.setMessage(true, 'Успешное пополнение счета на' + balanceInc.currency + balanceInc.amount)
+        } else {
+            return moneyManager.setMessage(false, response.error)
+        }
+    })
+}
+
+// 2. Конвертирование валюты
+
+moneyManager.conversionMoneyCallback = function(convert) {
+    ApiConnector.convertMoney(convert, response => {
+        console.log(response);
+        if (response.success) {
+            moneyManager.conversionMoneyAction();
+            ProfileWidget.showProfile(response.data);
+            return moneyManager.setMessage(true, 'Успешная конвертация суммы ' + convert.fromCurrency + convert.fromAmount)
+        } else {
+            return moneyManager.setMessage(false, response.error)
+        }
+    })
+}
+
+// 3. Перевод валюты
+
+moneyManager.sendMoneyCallback = function(trans) {
+    ApiConnector.transferMoney(trans, response => {
+        if (response.success) {
+            moneyManager.sendMoneyAction();
+            ProfileWidget.showProfile(response.data);
+            return moneyManager.setMessage(true, 'Успешный перевод ' + trans.currency + trans.amount + ' получателю ' + trans.to)
+        } else {
+            return moneyManager.setMessage(false, response.error)
+        }
+    })
+}
